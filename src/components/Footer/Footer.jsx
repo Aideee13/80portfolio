97f0941dd1f779eeae0f37c8c09c8ed5ec0bbe7b
Aideee13 <124105395+Aideee13@users.noreply.github.com>
2023-03-28 19:23:05 -0400

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -17,18 +17,18 @@ const Footer = () => {
       className={`innerWidth yPaddings flexCenter ${css.container}`}>
         <div className={css.left}>
           <span className="primaryText">
-            Let's make something <br />
-            amazing together.
+            Let's create <br />
+            together.
           </span>
           <span className="primaryText">
-            Start by <a href="mailto:zainkeepscode@gmail.com">saying hi</a>
+            Start by <a href="mailto:aide1013@gmail.com">saying hi</a>
           </span>
         </div>
 
         <div className={css.right}>
           <div className={css.info}>
             <span className="secondaryText">Information</span>
-            <p>145 New York, FL 5467, USA</p>
+            <p>Bridgeport,CT 06610 USA</p>
           </div>
           <ul className={css.menu}>
             <li>Services</li>
