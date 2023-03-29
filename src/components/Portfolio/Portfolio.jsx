e3c923288903161fa92920e30244420caa68d228
Aideee13 <124105395+Aideee13@users.noreply.github.com>
2023-03-28 20:37:35 -0400

    Update Portfolio.jsx

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -18,10 +18,10 @@ const Portfolio = () => {
 
         <motion.div variants={textVariant(.4)} className={`flexCenter ${css.heading}`}>
           <div>
-            <span className="primaryText">My Latest Works</span>
-            <p style={{marginTop: "10px"}}>Perfect solution for digital experience</p>
+            <span className="primaryText">Recent Work</span>
+            <p style={{marginTop: "10px"}}>Digital experience</p>
           </div>
-          <span className="secondaryText">Explore More Works</span>
+          <span className="secondaryText">Explore</span>
         </motion.div>
 
 
